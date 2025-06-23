db1c7e8fc7229127e955ebc3c04f711685f19063
TheAlchemist010 <daniel1gallagher1@gmail.com>
2025-06-23 10:54:15 +1000

    Added sokuban generation

---
+++ sokuban_gen/generate.py
@@ -0,0 +1,108 @@
+def sokoban_to_pddl(map_str, problem_name="p146-microban-sequential"):
+    lines = [line for line in map_str.strip().split("\n")]
+    height, width = len(lines), max(len(line) for line in lines)
+
+    walls = set()
+    goals = set()
+    stones = []
+    player_pos = None
+
+    def pos_name(r, c):
+        return f"pos-{r + 1}-{c + 1}"
+
+    locations = []
+    for r, line in enumerate(lines):
+        for c, ch in enumerate(line.ljust(width)):
+            locations.append((r, c))
+            if ch == "#":
+                walls.add((r, c))
+            elif ch == ".":
+                goals.add((r, c))
+            elif ch == "$":
+                stones.append((r, c))
+            elif ch == "@":
+                player_pos = (r, c)
+            elif ch == "*":  # stone on goal
+                stones.append((r, c))
+                goals.add((r, c))
+            elif ch == "+":  # player on goal
+                player_pos = (r, c)
+                goals.add((r, c))
+            elif ch == "?":  # alternate for block
+                stones.append((r, c))
+
+    location_names = {pos: pos_name(*pos) for pos in locations}
+    output = []
+
+    output.append(f"(define (problem {problem_name})")
+    output.append("  (:domain sokoban-sequential)")
+    output.append("  (:objects")
+    output.append("    dir-down - direction")
+    output.append("    dir-left - direction")
+    output.append("    dir-right - direction")
+    output.append("    dir-up - direction")
+    output.append("    player-01 - player")
+
+    for pos in sorted(locations):
+        output.append(f"    {location_names[pos]} - location")
+    for i in range(len(stones)):
+        output.append(f"    stone-{i + 1:02d} - stone")
+    output.append("  )")
+
+    output.append("  (:init")
+    for goal in goals:
+        output.append(f"    (IS-GOAL {location_names[goal]})")
+    for pos in locations:
+        if pos not in goals:
+            output.append(f"    (IS-NONGOAL {location_names[pos]})")
+
+    for r, c in locations:
+        for dr, dc, dname in [
+            (-1, 0, "dir-up"),
+            (1, 0, "dir-down"),
+            (0, -1, "dir-left"),
+            (0, 1, "dir-right"),
+        ]:
+            nr, nc = r + dr, c + dc
+            if (nr, nc) in locations:
+                output.append(
+                    f"    (MOVE-DIR {location_names[(r, c)]} {location_names[(nr, nc)]} {dname})"
+                )
+
+    output.append(f"    (at player-01 {location_names[player_pos]})")
+
+    for i, pos in enumerate(stones):
+        output.append(f"    (at stone-{i + 1:02d} {location_names[pos]})")
+
+    for pos in locations:
+        if pos not in stones and pos != player_pos and pos not in walls:
+            output.append(f"    (clear {location_names[pos]})")
+
+    output.append("  )")
+
+    output.append("  (:goal (and")
+    for i in range(len(stones)):
+        output.append(f"    (at-goal stone-{i + 1:02d})")
+    output.append("  ))")
+
+    output.append(")")
+
+    return "\n".join(output)
+
+
+# Example usage:
+sokoban_input = """
+#@########
+#   ..# .#
+#       .#
+#  #   # #
+# #.## # #
+# #.#  # #
+#        #
+# $$#  # #
+#   #     
+##########
+"""
+
+pddl_output = sokoban_to_pddl(sokoban_input)
+print(pddl_output)
---
+++ sokuban_gen/navigation.py
@@ -0,0 +1,120 @@
+def parse_maze(maze_str):
+    maze_lines = [line for line in maze_str.strip().split("\n") if line]
+    height = len(maze_lines)
+    width = max(len(line) for line in maze_lines)
+    grid = []
+    start = None
+    goals = []
+    dangerous_goals = []
+
+    for y, line in enumerate(maze_lines):
+        row = []
+        for x, char in enumerate(line.ljust(width)):
+            if char == "#":
+                row.append({"type": "wall"})
+            elif char == " ":
+                row.append({"type": "open"})
+            elif char == "S":
+                row.append({"type": "open"})
+                start = (y, x)
+            elif char == "G":
+                row.append({"type": "open"})
+                goals.append((y, x))
+            elif char == "D":
+                row.append({"type": "open"})
+                dangerous_goals.append((y, x))
+            else:
+                row.append({"type": "wall"})  # Treat unknown characters as walls
+        grid.append(row)
+    return grid, start, goals, dangerous_goals, height, width
+
+
+def generate_pddl(
+    grid, start, goals, dangerous_goals, height, width, problem_name="maze-problem"
+):
+    def node_name(y, x):
+        return f"node{y}-{x}"
+
+    objects = []
+    init = []
+    places = []
+    opens = []
+    connections = []
+
+    for y in range(height):
+        for x in range(width):
+            cell = grid[y][x]
+            name = node_name(y, x)
+            objects.append(name)
+            places.append(f"(place {name})")
+            if cell["type"] == "open":
+                opens.append(f"(open {name})")
+            # Add connections to adjacent open cells
+            if cell["type"] == "open":
+                for dy, dx in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                    ny, nx = y + dy, x + dx
+                    if 0 <= ny < height and 0 <= nx < width:
+                        neighbor = grid[ny][nx]
+                        if neighbor["type"] == "open":
+                            connections.append(f"(conn {name} {node_name(ny, nx)})")
+
+    init.append(f"(at-robot {node_name(*start)} robot)")
+    for goal in goals:
+        init.append(f"(is-goal {node_name(*goal)})")
+    for d_goal in dangerous_goals:
+        init.append(f"(is-goal-dangerous {node_name(*d_goal)})")
+
+    pddl = f"(define (problem {problem_name})\n"
+    pddl += "  (:domain grid)\n"
+    pddl += "  (:objects\n    robot\n"
+    pddl += "    " + " ".join(objects) + "\n  )\n"
+    pddl += "  (:init\n"
+    pddl += "    " + "\n    ".join(init) + "\n"
+    pddl += "    " + "\n    ".join(places) + "\n"
+    pddl += "    " + "\n    ".join(opens) + "\n"
+    pddl += "    " + "\n    ".join(connections) + "\n"
+    pddl += "  )\n"
+    pddl += f"  (:goal (and (at-robot {node_name(*goals[0])} robot)))\n"
+    pddl += ")"
+    return pddl
+
+
+# Example usage:
+maze_input = """
+#############
+#S   #    G #
+# ##   #### #
+# ##   #### #
+#    #      #
+##  ##  # # #
+#       # # #
+# ## ## #G#D#
+# ## ## ### #
+#           #
+# ## ## ## ##
+# ##D##G## ##
+#############
+"""
+
+# maze_input = """
+###############
+# S     #  #G  #
+# ### ### ### #
+#   #         #
+### # ####### #
+#   #         #
+# ####### ### #
+#     #    #D #
+## # ## #######
+#  #        #G#
+# ######### # #
+#             #
+## #### #### ##
+#    G#   D   #
+###############
+# ""
+
+
+grid, start, goals, dangerous_goals, height, width = parse_maze(maze_input)
+pddl_output = generate_pddl(grid, start, goals, dangerous_goals, height, width)
+print(pddl_output)
