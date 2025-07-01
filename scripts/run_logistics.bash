520af6a840498485f9f9d71dd7d16e8d49496324
TheAlchemist010 <daniel1gallagher1@gmail.com>
2025-07-01 19:52:35 +1000

    .

--- scripts/run_logistics.bash
+++ scripts/run_logistics.bash
@@ -1,9 +1,21 @@
-java -cp out:lib/*:. com.suspiciousbehaviour.app.Main \
-  -a \
-  -o outputs/logistics \
-  -d PDDL/logistics/domain.pddl \
-  PDDL/logistics/logistics-F/problem-1.pddl \
-  PDDL/logistics/logistics-F/problem-2.pddl \
-  PDDL/logistics/logistics-F/problem-3.pddl \
-  PDDL/logistics/logistics-F/problem-4.pddl \
-  PDDL/logistics/logistics-F/problem-5.pddl
+#! /bin/bash
+problem='logistics-A'
+domain='logistics'
+
+mkdir outputs/$domain/$problem -p
+
+java -cp out:lib/*:. -Xmx11G com.suspiciousbehaviour.app.Main \
+  --directed \
+  --primary_goal 3 \
+  --obfuscating \
+  --loitering \
+  --purposefulE 0.4 \
+  --unexpected \
+  --secondary_goal 1 \
+  -o outputs/$domain/$problem \
+  -d PDDL/$domain/domain.pddl \
+  -p PDDL/$domain/$problem/core-problem.pddl \
+  PDDL/$domain/$problem/goal-1.pddl \
+  PDDL/$domain/$problem/goal-2.pddl \
+  PDDL/$domain/$problem/goal-3.pddl \
+  PDDL/$domain/$problem/goal-4.pddl
